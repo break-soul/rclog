bb6e11c25f963fcb3547abbe4f257fc73f9512df
break-soul <57186766+break-soul@users.noreply.github.com>
2023-01-05 19:32:23 +0800

    add notes

--- TransCoding.py
+++ TransCoding.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # TransCoding.py
 
+from typing import Any
 
 def dump_format(format_name: str = "default",
                 **kw: dict[str, str]) -> dict[str, dict[str, str]]:
@@ -72,7 +73,29 @@ def trans_config(handlers: list,
                      "default",
                  ],
                  exist_loggers: bool = True,
-                 **kw):
+                 **kw) -> dict[str, Any]:
+    """
+    trans config
+
+    Args:
+        handlers (list): _description_
+        formats (list, optional): _description_. Defaults to [ "default", ].
+        exist_loggers (bool, optional): _description_. Defaults to True.
+
+    Keyword Args:
+        "{format_name}_format" (str, optional): _description_. Defaults to "<%(asctime)s>[%(levelname)s]%(name)s:%(message)s".
+        "{format_name}_datefmt" (str, optional): _description_. Defaults to "%Y-%m-%d %H:%M:%S".
+        "{handler_name}_class" (str, optional): _description_. Defaults to "Console".
+        "{handler_name}_formatter" (str, optional): _description_. Defaults to "default".
+        "{handler_name}_level" (str, optional): _description_. Defaults to None.
+        "{handler_name}_filename" (str, optional): _description_. Defaults to None.
+        "{handler_name}_maxBytes" (int, optional): _description_. Defaults to None.
+        "{handler_name}_backupCount" (int, optional): _description_. Defaults to None.
+        "{handler_name}_encoding" (str, optional): _description_. Defaults to "utf8".
+
+    Returns:
+        dict[str, Any]: _description_
+    """
 
     # init config
     config = dict()
