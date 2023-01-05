3a2ca40144303e44cae35d15e08a489654442dde
break-soul <57186766+break-soul@users.noreply.github.com>
2023-01-05 19:14:11 +0800

    add from_args function

--- SetLogger.py
+++ SetLogger.py
@@ -4,9 +4,36 @@
 from logging.config import dictConfig
 
 from .GetLogger import get_logger
-from .TransCoding import trans_coding
+from .TransCoding import trans_config
 
 logger = get_logger("Logger.SetLogger")
+
+
+def from_dict(config_dict: dict):
+    """
+    从字典初始化日志配置
+
+    Args:
+        config_dict (dict): 配置字典
+    """
+
+    try:
+        dictConfig(trans_config(**config_dict))
+    except Exception as e:
+        logger.error("Failed to set logging config: %s" % e)
+
+
+def from_args(*args,**kw):
+    """
+    从函数参数初始化日志配置
+    """
+
+    try:
+        dictConfig(trans_config(*args,**kw))
+    except Exception as e:
+        logger.error("Failed to set logging config: %s" % e)
+        raise
+
 
 def from_object(obj: object):
     """
@@ -14,7 +41,7 @@ def from_object(obj: object):
 
     Args:
         obj (object): 配置对象
-    """    
+    """
 
     try:
         from .ImportString import import_string
@@ -24,19 +51,6 @@ def from_object(obj: object):
         for key in dir(log_config):
             if key.isupper():
                 config_dict[key] = getattr(log_config, key)
-        dictConfig(trans_coding(**config_dict))
-    except Exception as e:
-        logger.error("Failed to set logging config: %s" % e)
-
-def from_dict(config_dict: dict):
-    """
-    从字典初始化日志配置
-
-    Args:
-        config_dict (dict): 配置字典
-    """    
-
-    try:
-        dictConfig(trans_coding(**config_dict))
+        dictConfig(trans_config(**config_dict))
     except Exception as e:
         logger.error("Failed to set logging config: %s" % e)
